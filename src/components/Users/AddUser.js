823d06decbd59148e4a4978c1c09f2af7088a7d0
vsheva <valera.sheva@gmail.com>
2022-06-26 18:34:29 +0300

    add button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
+import Card from "../UI/Card.js"
+import Button from "../UI/Button.js"
+import classes from "./AddUser.module.css"
 
-const AddUser=(props)=> {
-    const addUserHandler=(e)=>{
+
+const AddUser = (props) => {
+    const addUserHandler = (e) => {
         e.preventDefault();
     }
 
     return (
-       <form onSubmit={addUserHandler}>
-           <label htmlFor="username">Username</label>
-           <input id="username" type="text" />
-           <label htmlFor="age">Age(Years</label>
-           <input id="age" type="number" />
-           <button type="submit">Add User</button>
-       </form>
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"/>
+                <label htmlFor="age">Age(Years</label>
+                <input id="age" type="number"/>
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
     );
 }
 
